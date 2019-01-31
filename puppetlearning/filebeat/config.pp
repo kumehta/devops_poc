6661df844827f2f802330ad1af6bcb402a5b98e5
Kunal Mehta <kunal.a.mehta@capgemini.com>
2019-01-31 11:24:31 +0530

    filebeat first draft

---
+++ puppetlearning/filebeat/config.pp
@@ -0,0 +1,67 @@
+# -----------------------------------------------------------------------------
+# Author  = Kunal Mehta (Capgemini on behalf of RMG.
+# Date    = 2019/01/30
+# Version = 1.0 'filebeat/config.pp'
+# Purpose = This is the DevOps Tooling Platform
+#           'filebeat/config' profile.
+#
+# -----------------------------------------------------------------------------
+
+class profile::devops::filebeat::config (
+  $config = undef,
+  String $filebeat_owner,
+  String $filebeat_group,
+  String $filebeat_config_file,
+  String $log_dirs,
+) {
+
+  # Set the global execution path_
+  Exec { path => ['/sbin', '/bin', '/usr/sbin', '/usr/bin'] }
+
+  $banner='# Elements of this file are managed by Puppet.'
+
+  # Patch the filebeat server configuration file.
+  file { "${filebeat_config_file}":
+    ensure  => file,
+    owner   => "$filebeat_owner",
+    group   => "$filebeat_group",
+    mode    => "0660",
+  } ->
+  exec { "${filebeat_config_file}":
+    command => "sed -i '1s/^/#${banner}\\n\\n/' ${filebeat_config_file}",
+    unless  => "grep '${banner}' ${filebeat_config_file}",
+  } ->
+  exec { "${filebeat_config_file} broker":
+    command     => "sed -i '23d' ${filebeat_config_file}",
+    refreshonly => true,
+    subscribe   => Exec[$filebeat_config_file],
+  } ->
+  file_line { "${filebeat_config_file} num_network_threads":
+    path    => "${filebeat_config_file}",
+    match   => "^num.network.threads",
+    line    => "num.network.threads=${num_network_threads}",
+    replace => true,
+    ensure  => present,
+  } ->
+  file {"/etc/systemd/system/filebeat.service":
+    mode    => '600',
+    owner   => root,
+    group   => root,
+    backup  => false,
+    content => template($filebeat_start_template),
+  } ->
+  exec { "Enable filebeat":
+    command => 'systemctl daemon-reload ; systemctl enable filebeat',
+    unless  => 'test -f /etc/systemd/system/multi-user.target.wants/filebeat.service',
+  } ->
+  service { "(Re)start filebeat":
+    name      => "filebeat",
+    enable    => true,
+    ensure    => running,
+    subscribe => File["${filebeat_config_file}"],
+  }
+}
+
+# -----------------------------------------------------------------------------
+# End of file.
+# -----------------------------------------------------------------------------
---
+++ puppetlearning/filebeat/install.pp
@@ -0,0 +1,62 @@
+# -----------------------------------------------------------------------------
+# Author  = Kunal Mehta (Capgemini on behalf of RMG.
+# Date    = 2019/01/30
+# Version = 1.0 'filebeat/install.pp'
+# Purpose = This is the DevOps Tooling Platform
+#           'filebeat/install' profile.
+#
+# -----------------------------------------------------------------------------
+
+class profile::devops::filebeat::install (
+  $config = undef,
+) {
+
+  $nexus_server  = hiera('profile::common::nexus')
+  $nexus_repo    = hiera('profile::devops::filebeat::install::nexus_repo')
+  $filebeat_package = hiera('profile::devops::filebeat::install::package')
+  $download_dir  = hiera('profile::devops::filebeat::install::download_dir')
+  $filebeat_dir     = hiera('profile::devops::filebeat::install::filebeat_dir')
+
+  # Determine filebeat version to be installed (V.R.M.F).
+  $vrmf = "$filebeat_package".match(/[0-9]+[\.]+[0-9]+[\-]+[0-9]+[\.]+[0-9]+[\.]+[0-9]+/)
+  $filebeat_version = $vrmf[0]
+
+  # Set the global execution path.
+  Exec { path => ['/sbin', '/bin', '/usr/sbin', '/usr/bin'] }
+  # Get configuration details.
+
+  exec { 'get_filebeat':
+    cwd         => "${download_dir}",
+    command     => "wget ${nexus_server}/${nexus_repo}/${filebeat_package} && touch ${download_dir}/.puppet/get_filebeat.done",
+    unless      => "test -e ${download_dir}/${filebeat_package}",
+    timeout     => 12000,
+    creates     => "${download_dir}/.puppet/get_filebeat.done"
+  } ->
+  exec { 'unzip_filebeat':
+     cwd         => "${download_dir}",
+     command     => "rpm -ivh ${filebeat_package} && touch ${download_dir}/.puppet/unzip_filebeat.done",
+     timeout     => 1200,
+     creates     => "${download_dir}/.puppet/unzip_filebeat.done",
+   } ->
+   exec { 'mv_filebeat_home_dir':
+    cwd         => "${download_dir}",
+    command     => "mv /usr/share/filebeat/* /app/filebeat/ && touch ${download_dir}/.puppet/mv_filebeat_home_dir.done",
+    timeout     => 1200,
+    creates     => "${download_dir}/.puppet/mv_filebeat.done",
+   } ->
+   exec { 'rm_filebeat_package':
+     cwd        => "${download_dir}",
+     command    => "rm -rf ${filebeat_package} && touch ${download_dir}/.puppet/rm_filebeat_package.done",
+     timeout    => 1200,
+     creates    => "${download_dir}/.puppet/rm_filebeat_package.done"
+   }
+   exec { 'change_ownership':
+    cwd         => "${filebeat_home_dir}",
+    command     => "chown -R filebeat:filebeat ${filebeat_home_dir}  && touch ${download_dir}/.puppet/change_filebeat_ownership.done",
+    creates     => "${download_dir}/.puppet/change_filebeat_ownership.done",
+   }
+}
+
+# -----------------------------------------------------------------------------
+# End of file.
+# -----------------------------------------------------------------------------
